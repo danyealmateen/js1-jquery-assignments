42ec59a8503dcdb4043e07508d32519c21e553c6
danyealmateen <danyeal.mateen@hotmail.com>
2022-12-05 14:08:28 +0100

    asd

--- script.js
+++ script.js
@@ -50,92 +50,112 @@
 //   console.log("H1");
 // });
 
-//b) Lägg till en eventlistener på p-element som lyssnar efter klick. När ett klick registreras ska “P” loggas i konsolen.
-const para = document.querySelectorAll("p");
-$("p").click(() => {
-  console.log("P");
-});
+// //b) Lägg till en eventlistener på p-element som lyssnar efter klick. När ett klick registreras ska “P” loggas i konsolen.
+// const para = document.querySelectorAll("p");
+// $("p").click(() => {
+//   console.log("P");
+// });
 
-//c) Lägg till en eventlistener på knapp-element som lyssnar efter klick. När ett klick registreras ska “KNAPP” loggas i konsolen.
-const button = document.querySelectorAll("button");
-$("button").click(() => {
-  console.log("KNAPP");
-});
+// //c) Lägg till en eventlistener på knapp-element som lyssnar efter klick. När ett klick registreras ska “KNAPP” loggas i konsolen.
+// // const button = document.querySelectorAll("button");
+// // $("button").click(() => {
+// //   console.log("KNAPP");
+// // });
 
-//Ändra H2
-//a) Modifiera response-funktionerna från första uppgiften så att texten för h2-elementet ändras.
-//b) Klickar man på h1-elementet ska texten h2-elementet vara “Du klickade på H1!”
-const header = document.querySelectorAll("h1");
-const headerTwo = document.querySelectorAll("h2");
-$("h1").click(() => {
-  $("h2").text("Du klickade på H1!");
-});
+// //Ändra H2
+// //a) Modifiera response-funktionerna från första uppgiften så att texten för h2-elementet ändras.
+// //b) Klickar man på h1-elementet ska texten h2-elementet vara “Du klickade på H1!”
+// const header = document.querySelectorAll("h1");
+// const headerTwo = document.querySelectorAll("h2");
+// $("h1").click(() => {
+//   $("h2").text("Du klickade på H1!");
+// });
 
-//c) Klickar man på p-elementet ska texten h2-elementet vara “Du klickade på P!”
-$("p").click(() => {
-  $("h2").text("Du klickade på P!");
-});
+// //c) Klickar man på p-elementet ska texten h2-elementet vara “Du klickade på P!”
+// $("p").click(() => {
+//   $("h2").text("Du klickade på P!");
+// });
 
-//d) Klickar man på knappen ska texten h2-elementet vara “Du klickade på knappen!”
-$("button").click(() => {
-  $("h2").text("Du klickade på knappen");
-});
+// //d) Klickar man på knappen ska texten h2-elementet vara “Du klickade på knappen!”
+// $("button").click(() => {
+//   $("h2").text("Du klickade på knappen");
+// });
 
-//3.Ändra bakgrundsfärg
-// Lägg till ytterligare en sak till response-funktionerna från första uppgiften.
-// Klickar man på h1-elementet ska bodyns bakgrundsfärg bli ljusblå.
-$("h1").click(() => {
-  $("button").css("backgroundColor", "purple");
-});
+// //3.Ändra bakgrundsfärg
+// // Lägg till ytterligare en sak till response-funktionerna från första uppgiften.
+// // Klickar man på h1-elementet ska bodyns bakgrundsfärg bli ljusblå.
+// $("h1").click(() => {
+//   $("button").css("backgroundColor", "purple");
+// });
 
-// Klickar man på p-elementet ska bodyns bakgrundsfärg bli rosa.
-$("p").click(() => {
-  $("body").css("backgroundColor", "hotpink");
-});
+// // Klickar man på p-elementet ska bodyns bakgrundsfärg bli rosa.
+// $("p").click(() => {
+//   $("body").css("backgroundColor", "hotpink");
+// });
 
-// Klickar man på knappen ska bodyns bakgrundsfärg bli orange.
-$("button").click(() => {
-  $("body").css("backgroundColor", "orange");
-});
+// // Klickar man på knappen ska bodyns bakgrundsfärg bli orange.
+// // $("button").click(() => {
+// //   $("body").css("backgroundColor", "orange");
+// // });
 
-//MOUSEENTER / MOUSELEAVE
-//1. h1
-//a) Lägg till en eventlistener på h1-elementet som lyssnar efter ett mouseenter-event. När det eventet-sker ska textfärgen på h1-elementet ändras.
-$("h1").mouseenter(() => {
-  $("h1").css("color", "blue");
-});
-//b) Lägg till en eventlistener på h1-elementet som lyssnar efter ett mouseleave-event. När det eventet-sker ska textfärgen på h1-elementet ändras.
-$("h1").mouseenter(() => {
-  $("h1").css("color", "red");
-});
+// //MOUSEENTER / MOUSELEAVE
+// //1. h1
+// //a) Lägg till en eventlistener på h1-elementet som lyssnar efter ett mouseenter-event. När det eventet-sker ska textfärgen på h1-elementet ändras.
+// $("h1").mouseenter(() => {
+//   $("h1").css("color", "blue");
+// });
+// //b) Lägg till en eventlistener på h1-elementet som lyssnar efter ett mouseleave-event. När det eventet-sker ska textfärgen på h1-elementet ändras.
+// $("h1").mouseenter(() => {
+//   $("h1").css("color", "red");
+// });
 
-//2. P
-//a) Lägg till en eventlistener på p-elementet som lyssnar efter ett mouseenter-event. När det eventet-sker ska textfärgen på knappen ändras.
-$("p").mouseenter(() => {
-  $("button").css("color", "yellow");
-});
-//b) Lägg till en eventlistener på p-elementet som lyssnar efter ett mouseleave-event. När det eventet-sker ska textfärgen på knappen ändras.
-$("p").mouseenter(() => {
-  $("button").css("color", "green");
-});
+// //2. P
+// //a) Lägg till en eventlistener på p-elementet som lyssnar efter ett mouseenter-event. När det eventet-sker ska textfärgen på knappen ändras.
+// $("p").mouseenter(() => {
+//   $("button").css("color", "yellow");
+// });
+// //b) Lägg till en eventlistener på p-elementet som lyssnar efter ett mouseleave-event. När det eventet-sker ska textfärgen på knappen ändras.
+// $("p").mouseenter(() => {
+//   $("button").css("color", "green");
+// });
 
-//3. Button
-//a) Lägg till en eventlistener på knappen som lyssnar efter ett mouseenter-event. När det eventet-sker ska texten på h1-elementet ändras till “Knappen!”
-$("button").mouseenter(() => {
-  $("h1").text("Knappen!");
-});
+// //3. Button
+// //a) Lägg till en eventlistener på knappen som lyssnar efter ett mouseenter-event. När det eventet-sker ska texten på h1-elementet ändras till “Knappen!”
+// $("button").mouseenter(() => {
+//   $("h1").text("Knappen!");
+// });
 
-//b)Lägg till en eventlistener på knappen som lyssnar efter ett mouseleave-event. När det eventet-sker ska texten på h1-elementet ändras tillbaka till “H1-elementet”
-$("button").mouseleave(() => {
-  $("h1").text("H1-Elementet");
-});
+// //b)Lägg till en eventlistener på knappen som lyssnar efter ett mouseleave-event. När det eventet-sker ska texten på h1-elementet ändras tillbaka till “H1-elementet”
+// $("button").mouseleave(() => {
+//   $("h1").text("H1-Elementet");
+// });
 
 //1. Bodyn
 //a) Lägg till en eventlistner på body-elementet som lyssnar efter mousemove.
 //b) Använd muspekarens y-position, alltså event.clientY för att ändra på h1-elementets font-storlek
-$("body").mousemove((e) => {
-  e.pageY;
-  console.log(e.pageY);
-  $("h1").css("fontSize", "50px");
-});
+// $("body").mousemove((e) => {
+//   e.pageY;
+//   console.log(e.pageY);
+//   $("h1").css("fontSize", "50px");
+// });
 //c) Använd muspekarens x-position, alltså event.clientX för att ändra på bakgrundsfärgen för p-elementet.
+// $("body").mousemove((e) => {
+//   e.pageX;
+//   $("p").css("backgroundColor", "beige");
+// });
+
+//Forms
+//1. Lägg till element
+//a)
+let form = document.querySelectorAll("form");
+let button = document.querySelectorAll("button");
+
+$("button").click((event) => {
+  event.preventDefault();
+  let input = document.querySelector("input").value;
+  console.log(input);
+  let header = document.createElement("h1");
+  header.innerHTML = `${input}`;
+  $("body").append(header);
+  console.log(header);
+});
